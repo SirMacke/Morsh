796dd43dd91bf69da0b7e67080c7292b06ee324e
Maximilian <max.helmersson@gmail.com>
2020-11-26 21:04:53 +0100

    server static issues

--- server/startup/routes.js
+++ server/startup/routes.js
@@ -5,8 +5,8 @@ const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-/*const serveStatic = require('serve-static');
-const history = require('connect-history-api-fallback');
+const serveStatic = require('serve-static');
+/*const history = require('connect-history-api-fallback');
 const enforce = require('express-sslify');*/
 
 const cookieParser = require('cookie-parser');
@@ -14,12 +14,12 @@ const home = require('../routes/api/home');
 const error = require('../middleware/error');
 
 module.exports = function(app) {
-  if (process.env.NODE_ENV == 'production') {
+  /*if (process.env.NODE_ENV == 'production') {
     app.use(express.static('client/dist'));
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
     });
-  }
+  }*/
 
   app.use(cors());
   app.use(morgan('tiny'));
@@ -31,9 +31,9 @@ module.exports = function(app) {
   }));
   app.use(cookieParser());
 
-  /*app.use(enforce.HTTPS({ trustProtoHeader: true }));
+  //app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(serveStatic(__dirname + '/dist'));
-  app.use(history());*/
+  //app.use(history());
 
   app.use('/api/home', home);
 
